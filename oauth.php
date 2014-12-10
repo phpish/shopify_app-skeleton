a4ae8f56e68f30d7e4b8ec21c6dfb8f88ac064ea
Sandeep Shetty <sandeep.shetty@gmail.com>
2014-12-10 18:24:56 +0530

    Upgraded to the latest version of phpish/shopify where shopify\access_token() directly returns the oauth access token.

--- oauth.php
+++ oauth.php
@@ -23,8 +23,7 @@
 	try
 	{
 		# shopify\access_token can throw an exception
-		$response = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
-		$oauth_token = $response['access_token'];
+		$oauth_token = shopify\access_token($_GET['shop'], SHOPIFY_APP_API_KEY, SHOPIFY_APP_SHARED_SECRET, $_GET['code']);
 
 		$_SESSION['oauth_token'] = $oauth_token;
 		$_SESSION['shop'] = $_GET['shop'];
